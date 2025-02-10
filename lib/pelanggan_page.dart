ae8f5d2b15c1cafdcdb24ef3795c1208732daab2
Natasyaaurellya <natasya.xpplg2@gmail.com>
2025-02-10 07:53:53 +0700

    crud pelanggan

--- lib/pelanggan_page.dart
+++ lib/pelanggan_page.dart
@@ -2,81 +2,241 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class PelangganPage extends StatefulWidget {
+  final VoidCallback? onCustomerUpdated; // Ubah menjadi nullable (opsional)
+
+  const PelangganPage({Key? key, this.onCustomerUpdated}) : super(key: key);
+
   @override
-  _PelangganPageState createState() => _PelangganPageState();
+  _CustomerPageState createState() => _CustomerPageState();
 }
 
-class _PelangganPageState extends State<PelangganPage> {
+class _CustomerPageState extends State<PelangganPage> {
   final SupabaseClient supabase = Supabase.instance.client;
-  List<Map<String, dynamic>> pelangganList = [];
+  List<Map<String, dynamic>> customers = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    fetchPelanggan();
+    _fetchCustomers();
   }
 
-  Future<void> fetchPelanggan() async {
-    final response = await supabase.from('pelanggan').select();
-    setState(() {
-      pelangganList = List<Map<String, dynamic>>.from(response);
-    });
+  Future<void> _fetchCustomers() async {
+    try {
+      final response = await supabase.from('pelanggan').select();
+      setState(() {
+        customers = List<Map<String, dynamic>>.from(response);
+        isLoading = false;
+      });
+    } catch (e) {
+      _showError('Failed to fetch customers: $e');
+    }
   }
 
-  Future<void> addPelanggan(
+  Future<void> _addCustomer(
       String nama, String alamat, String nomorTelepon) async {
-    await supabase.from('pelanggan').insert({
-      'nama_pelanggan': nama,
-      'alamat': alamat,
-      'nomor_telepon': nomorTelepon,
-    });
-    fetchPelanggan();
+    try {
+      await supabase.from('pelanggan').insert({
+        'nama_pelanggan': nama,
+        'alamat': alamat,
+        'nomor_telepon': nomorTelepon,
+      });
+      _fetchCustomers();
+
+    } catch (e) {
+      _showError('Failed to add customer: $e');
+    }
   }
 
-  Future<void> deletePelanggan(int id) async {
-    await supabase.from('pelanggan').delete().eq('pelanggan_id', id);
-    fetchPelanggan();
+  Future<void> _updateCustomer(
+      int id, String nama, String alamat, String nomorTelepon) async {
+    try {
+      await supabase.from('pelanggan').update({
+        'nama_pelanggan': nama,
+        'alamat': alamat,
+        'nomor_telepon': nomorTelepon,
+      }).eq('pelanggan_id', id);
+      _fetchCustomers();
+// Panggil callback
+    } catch (e) {
+      _showError('Failed to update customer: $e');
+    }
   }
 
-  void showAddPelangganDialog() {
-    TextEditingController namaController = TextEditingController();
-    TextEditingController alamatController = TextEditingController();
-    TextEditingController nomorTeleponController = TextEditingController();
+  Future<void> _confirmDeleteCustomer(int id) async {
+    final result = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Konfirmasi'),
+          content:
+              const Text('Apakah Anda yakin ingin menghapus pelanggan ini?'),
+          actions: [
+            TextButton(
+              onPressed: () =>
+                  Navigator.pop(context, false), // Tidak jadi hapus
+              child: const Text('Tidak'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context, true), // Lanjutkan hapus
+              child: const Text('Ya'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (result == true) {
+      try {
+        await supabase.from('pelanggan').delete().eq('pelanggan_id', id);
+        _fetchCustomers();
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Pelanggan berhasil dihapus')),
+        );
+      } catch (e) {
+        _showError('Gagal menghapus pelanggan: $e');
+      }
+    }
+  }
+
+  void _showError(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  Widget _buildCustomerTable() {
+    return isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : customers.isEmpty
+            ? const Center(child: Text('No customers found'))
+            : SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  columns: const [
+                    DataColumn(label: Text('No.')),
+                    DataColumn(label: Text('Nama')),
+                    DataColumn(label: Text('Alamat')),
+                    DataColumn(label: Text('Nomor Telepon')),
+                    DataColumn(label: Text('Actions')),
+                  ],
+                  rows: customers.asMap().entries.map((entry) {
+                    final index = entry.key + 1;
+                    final customer = entry.value;
+                    return DataRow(cells: [
+                      DataCell(Text(index.toString())),
+                      DataCell(Text(customer['nama_pelanggan'])),
+                      DataCell(Text(customer['alamat'])),
+                      DataCell(Text(customer['nomor_telepon'])),
+                      DataCell(Row(
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () => _editCustomerDialog(customer),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () => _confirmDeleteCustomer(
+                                customer['pelanggan_id']),
+                          ),
+                        ],
+                      )),
+                    ]);
+                  }).toList(),
+                ),
+              );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Customers'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: _buildCustomerTable(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addCustomerDialog();
+        },
+        backgroundColor: const Color.fromARGB(255, 255, 178, 240),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _addCustomerDialog() {
+    final _formKey = GlobalKey<FormState>();
+    final TextEditingController namaController = TextEditingController();
+    final TextEditingController alamatController = TextEditingController();
+    final TextEditingController nomorTeleponController =
+        TextEditingController();
+    
 
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text('Tambah Pelanggan'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: namaController,
-                decoration: InputDecoration(labelText: 'Nama Pelanggan'),
+          title: const Text('Add Customer'),
+          content: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    controller: namaController,
+                    decoration: const InputDecoration(labelText: 'Nama'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nama tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: alamatController,
+                    decoration: const InputDecoration(labelText: 'Alamat'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Alamat tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: nomorTeleponController,
+                    decoration:
+                        const InputDecoration(labelText: 'Nomor Telepon'),
+                    keyboardType: TextInputType.phone,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nomor telepon tidak boleh kosong';
+                      }
+                      if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+                        return 'Nomor telepon harus berupa angka';
+                      }
+                      return null;
+                    },
+                  ),
+                 
+
+                ],
               ),
-              TextField(
-                controller: alamatController,
-                decoration: InputDecoration(labelText: 'Alamat'),
-              ),
-              TextField(
-                controller: nomorTeleponController,
-                decoration: InputDecoration(labelText: 'Nomor Telepon'),
-                keyboardType: TextInputType.phone,
-              ),
-            ],
+            ),
           ),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
-              child: Text('Batal'),
+              child: const Text('Batal'),
             ),
             ElevatedButton(
               onPressed: () {
-                if (namaController.text.isNotEmpty &&
-                    alamatController.text.isNotEmpty &&
-                    nomorTeleponController.text.isNotEmpty) {
-                  addPelanggan(
+                if (_formKey.currentState!.validate()) {
+                  // Validasi berhasil
+                  _addCustomer(
                     namaController.text,
                     alamatController.text,
                     nomorTeleponController.text,
@@ -84,7 +244,7 @@ class _PelangganPageState extends State<PelangganPage> {
                   Navigator.pop(context);
                 }
               },
-              child: Text('Tambah'),
+              child: const Text('Tambah'),
             ),
           ],
         );
@@ -92,31 +252,84 @@ class _PelangganPageState extends State<PelangganPage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Data Pelanggan')),
-      body: pelangganList.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: pelangganList.length,
-              itemBuilder: (context, index) {
-                final pelanggan = pelangganList[index];
-                return ListTile(
-                  title: Text(pelanggan['nama_pelanggan'] ?? 'Tidak ada nama'),
-                  subtitle: Text(
-                      '${pelanggan['alamat'] ?? 'Tidak ada alamat'}\n${pelanggan['nomor_telepon'] ?? 'Tidak ada nomor'}'),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    onPressed: () => deletePelanggan(pelanggan['pelanggan_id']),
-                  ),
-                );
-              },
+  void _editCustomerDialog(Map<String, dynamic> customer) {
+    final _formKey = GlobalKey<FormState>();
+    final TextEditingController namaController =
+        TextEditingController(text: customer['nama_pelanggan']);
+    final TextEditingController alamatController =
+        TextEditingController(text: customer['alamat']);
+    final TextEditingController nomorTeleponController =
+        TextEditingController(text: customer['nomor_telepon']);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Edit Customer'),
+          content: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: namaController,
+                  decoration: const InputDecoration(labelText: 'Nama'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: alamatController,
+                  decoration: const InputDecoration(labelText: 'Alamat'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Alamat tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: nomorTeleponController,
+                  decoration: const InputDecoration(labelText: 'Nomor Telepon'),
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nomor telepon tidak boleh kosong';
+                    }
+                    if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+                      return 'Nomor telepon harus berupa angka';
+                    }
+                    return null;
+                  },
+                ),
+              ],
             ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: showAddPelangganDialog,
-      ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Batal'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _updateCustomer(
+                    customer['pelanggan_id'],
+                    namaController.text,
+                    alamatController.text,
+                    nomorTeleponController.text,
+                  );
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Simpan'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
